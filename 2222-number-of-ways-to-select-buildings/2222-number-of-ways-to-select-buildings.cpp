7548d3fd672341a1bed1f1018da6cfde5c296bf0
abelkm99 <41730180+abelkm99@users.noreply.github.com>
2023-02-02 11:40:47 +0300

    Time: 1709 ms (5.27%), Space: 116.9 MB (11.72%) - LeetHub

---
+++ 2222-number-of-ways-to-select-buildings/2222-number-of-ways-to-select-buildings.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    long long numberOfWays(string s) {
+        vector<int> nums;
+        for (int i = 0; i < s.size(); i++) {
+            nums.push_back(s[i] - '0');
+        }
+        
+        long long dp[100005][3][3];
+        memset(dp, -1, sizeof(dp));
+        function<long long(int,int,int)> solve;
+        solve = [&](int idx, int prev, int cnt) {
+            if(cnt == 3) {
+                return 1LL;
+            }
+            if(idx == nums.size()) {
+                return 0LL;
+            }
+            if(dp[idx][prev][cnt] != -1) {
+                return dp[idx][prev][cnt];
+            }
+            long long can = 0;
+            if(nums[idx] != prev) {
+                can = solve(idx+1, nums[idx], cnt+1);
+            }
+            long long dont = solve(idx+1, prev, cnt);
+            return dp[idx][prev][cnt] = can + dont;
+        };
+        return solve(0, 2, 0);
+    }
+};
