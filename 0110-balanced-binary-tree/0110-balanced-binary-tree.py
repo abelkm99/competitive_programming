6bdf6d634d4e62f053d47c9e8339907b8456d6b5
Abel Kidanemariam <41730180+abelkm99@users.noreply.github.com>
2024-10-14 11:28:40 +0300

    Time: 45 ms (57.46%), Space: 17.8 MB (7.12%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -7,23 +7,14 @@
 class Solution:
     def isBalanced(self, root: Optional[TreeNode], ans: bool = True) -> bool:
 
-
-        ans = True
-
-        def sol(root):
-            nonlocal ans
+        def sol(root: TreeNode | None) -> tuple[int, bool]:
             if not root:
-                return 0
-        
+                return 0, True
         
             lc = sol(root.left)
             rc = sol(root.right)
 
-            if abs(lc - rc) > 1:
-                ans = False
-        
-            return max(lc, rc) + 1
-            
-        sol(root)
 
-        return ans
+            return (max(lc[0], rc[0]) + 1), lc[1] and rc[1] and abs(rc[0] - lc[0]) <= 1
+
+        return sol(root)[1]
