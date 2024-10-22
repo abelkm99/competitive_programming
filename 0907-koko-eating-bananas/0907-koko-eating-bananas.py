68388b4c5683785e36234d3e8b89dae72cdece03
Abel Kidanemariam <41730180+abelkm99@users.noreply.github.com>
2024-10-22 21:15:28 +0300

    Time: 214 ms (95.12%), Space: 17.8 MB (96.22%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.py
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.py
@@ -1,11 +1,14 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         def can(k: int) -> bool:
-            return sum([(x + k - 1) // k for x in piles]) <= h
-        l, r = 1, 10000000000
+            tmp = 0
+            for x in piles:
+                tmp += ((x + k - 1) // k)
+            return tmp <= h
+        l, r = 1, 100000000000
         ans = r
         while l <= r:
-            k = (l + r) // 2
+            k = (r - l) // 2 + l
             if can(k):
                 ans = k
                 r = k - 1
