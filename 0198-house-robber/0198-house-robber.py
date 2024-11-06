31b1c83243a76bdd4c7092c95224d927f69269b4
Abel Kidanemariam <41730180+abelkm99@users.noreply.github.com>
2024-11-06 15:43:19 +0300

    Time: 0 ms (100%), Space: 16.6 MB (22.24%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,11 +1,9 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-
-
-        @lru_cache
-        def dp(idx):
-            if idx >= len(nums):
-                return 0
-            return max(dp(idx + 2) + nums[idx], dp(idx + 1))
-        
-        return dp(0)
+        dp = [0] * len(nums)
+        # by the time i am there i should have gotten the max so far
+        ans = 0
+        for i in range(len(nums)):
+            dp[i] = max((dp[i - 2] if i - 2 >= 0 else 0 )+ nums[i], dp[i - 1] if i - 1 >= 0 else 0)
+            ans = max(ans, dp[i])
+        return ans
