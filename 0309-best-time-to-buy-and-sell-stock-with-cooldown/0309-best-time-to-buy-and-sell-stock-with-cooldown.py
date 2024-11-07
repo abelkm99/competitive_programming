5378461f044b94258bd5e84f8d5facaa7800fa3d
Abel Kidanemariam <41730180+abelkm99@users.noreply.github.com>
2024-11-07 22:21:27 +0300

    Time: 6 ms (48.7%), Space: 20.7 MB (6.71%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -3,12 +3,15 @@ class Solution:
         # class do this and do that how do i use.
         @cache
         def solve(idx: int, state: int) -> int:
-            if idx == len(prices):
+            if idx >= len(prices):
                 return 0
+
+            # i have the option to not do shit on this one i guess
+            cool_down = solve(idx + 1, state) # cool down by one
+
             if state == 0:
-                return max(solve(idx + 1, 1) - prices[idx], solve(idx + 1, state))
-            if state == 1:
-                return max(solve(idx + 1, 2) + prices[idx], solve(idx + 1, state))
-            return solve(idx + 1, 0)
+                return max(solve(idx + 1, 1) - prices[idx], cool_down)
+
+            return max(solve(idx + 2, 0) + prices[idx], cool_down)
         
         return solve(0, 0)
