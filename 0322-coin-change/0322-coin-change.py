d37f8898e54c68574574e2dfc95e3e97603690f9
Abel Kidanemariam <41730180+abelkm99@users.noreply.github.com>
2024-11-07 14:11:07 +0300

    Time: 2036 ms (5.01%), Space: 22.2 MB (20.36%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,14 +1,17 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        
+        dp = [[float("inf") for _ in range(len(coins))] for _ in range(amount + 1)]
+        # if don't pick this one
+        for rem in range(amount + 1):
+            for idx in range(len(coins)):
+                if rem == 0:
+                    dp[rem][idx] = 0
+                    continue
+                
+                case_1 = dp[rem][idx - 1] if idx - 1 >= 0 else float("inf")
+                case_2 = dp[rem - coins[idx]][idx] + 1 if rem -coins[idx] >= 0 else float("inf")
 
-        @cache
-        def solve(idx:int, rem: int):
-            if rem == 0:
-                return 0
-            if rem < 0 or idx == len(coins):
-                return float("inf")
-            return min(solve(idx + 1, rem), solve(idx, rem - coins[idx]) + 1)
-        
-        ans = solve(0, amount)
+                dp[rem][idx] = min(case_1, case_2)
+
+        ans = dp[amount][len(coins) - 1]
         return ans if ans != float("inf") else -1
