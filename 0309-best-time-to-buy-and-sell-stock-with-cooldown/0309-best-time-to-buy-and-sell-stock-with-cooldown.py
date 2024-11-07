1ce8a4422bbeacd5eb2a44734c6b49a1c702b7aa
Abel Kidanemariam <41730180+abelkm99@users.noreply.github.com>
2024-11-07 21:59:47 +0300

    Time: 7 ms (45.21%), Space: 21.9 MB (5.88%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -2,23 +2,13 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         # class do this and do that how do i use.
         @cache
-        def solve(idx: int, prev: int ,state: int) -> int:
-            # if state 0 -> means i can buy or not (i cant sell)
-            # if state 1 -> i've bought i can sell or not
-            # if state 2 ->  means i am cooling of so i can't do shit
-            # ballance
+        def solve(idx: int, state: int) -> int:
             if idx == len(prices):
-                # if it's finished return the previous bought data
-                return -1 * prev
-            # the state matters a lot
+                return 0
             if state == 0:
-                # buy the current or don't buy
-                return max(solve(idx + 1, prices[idx], 1), solve(idx + 1, prev, state))
+                return max(solve(idx + 1, 1) - prices[idx], solve(idx + 1, state))
             if state == 1:
-                # means i can seel it or don't sell it
-                # if i sell it change the state to cool off period
-                return max(solve(idx + 1, 0, 2) + prices[idx] - prev, solve(idx + 1, prev, state))
-            # means the state is 2 with an option to buy a stock
-            return solve(idx + 1, 0, 0)
+                return max(solve(idx + 1, 2) + prices[idx], solve(idx + 1, state))
+            return solve(idx + 1, 0)
         
-        return solve(0, 0, 0)
+        return solve(0, 0)
