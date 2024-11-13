4d761196a95f4106b6bc4f5ee5a2d184994585c0
Abel Kidanemariam <41730180+abelkm99@users.noreply.github.com>
2024-11-13 12:07:29 +0300

    Time: 138 ms (54.45%), Space: 38.8 MB (32.38%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -1,38 +1,31 @@
-from heapq import heappush, heappop
+
 class MedianFinder:
 
     def __init__(self):
-        self.mn = []
-        self.mx = []
+        self.heaps = [], []
         
 
     def addNum(self, num: int) -> None:
-        heappush(self.mx, -1 * num)
+        smaller, larger = self.heaps
 
-        # as long as the values on the max heap is minimum than the smallest value on the # mn heap
+        heappush(smaller, -1 * num)
+        heappush(larger, -1 * heappop(smaller))
 
-        # print(self.mn, self.mx)
-        while self.mx and self.mn and -1 * self.mx[0] > self.mn[0]:
-            heappush(self.mn, -1 * heappop(self.mx))
 
-        while abs(len(self.mn) - len(self.mx)) > 1:
-            if len(self.mn) > len(self.mx):
-                heappush(self.mx, -1 * heappop(self.mn))
-            else:
-                heappush(self.mn, -1 * heappop(self.mx))
-
+        if len(larger) - len(smaller) > 1:
+            heappush(smaller, -1 * heappop(larger))
+        
+        # print("add ",smaller, larger)
 
     def findMedian(self) -> float:
-        # print("ans",self.mn, self.mx)
+        smaller, larger = self.heaps
 
+        # print(larger, smaller)
 
-        if (len(self.mn) + len(self.mx)) & 1:
-            if len(self.mn) > len(self.mx):
-                return self.mn[0]
-            else:
-                return -1 * self.mx[0]
-        
-        return (-1 * self.mx[0] + self.mn[0]) / 2
+        if len(larger) > len(smaller):
+            return larger[0]
+
+        return (larger[0] + (smaller[0] * -1)) / 2
         
 
 
